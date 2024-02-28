b9a54eb69dd67c5f1f39ce520d70b1ffaba03853
Naturdragon <63045626+Naturdragon@users.noreply.github.com>
2024-02-28 14:08:24 +0100

    Added the Helper Folder with the HelperFilter Class

---
+++ src/main/java/at/ac/fhcampuswien/fhmdb/HelperClasses/FilterHelper.java
@@ -0,0 +1,5 @@
+package at.ac.fhcampuswien.fhmdb.HelperClasses;
+
+public class FilterHelper {
+    // A Helper Class containing methods for filtering. They can be accessed by creating an object of the class.
+}
